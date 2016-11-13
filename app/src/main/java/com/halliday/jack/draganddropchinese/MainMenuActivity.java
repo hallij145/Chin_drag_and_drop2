74612e18633d862e7e570e3da76039ba76dde777
Charlie Calder <cc9431@bard.edu>
2016-11-13 17:02:03 -0500

    First Commit

--- app/src/main/java/com/halliday/jack/draganddropchinese/MainMenuActivity.java
+++ app/src/main/java/com/halliday/jack/draganddropchinese/MainMenuActivity.java
@@ -2,12 +2,22 @@ package com.halliday.jack.draganddropchinese;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainMenuActivity extends AppCompatActivity {
+    private Button mStartButton;
+    private Button mDictionaryButton;
+    private Button mSettingsButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mStartButton = (Button) findViewById(R.id.StartButton);
+        mDictionaryButton = (Button) findViewById(R.id.Dictionary);
+        mSettingsButton = (Button) findViewById(R.id.Settings);
+
     }
 }
