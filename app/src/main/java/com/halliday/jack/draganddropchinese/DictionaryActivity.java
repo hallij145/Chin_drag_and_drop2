c56027c37d30c18277f52df9c4ec15cf2b1b3112
hallij145 <hallij145@gmail.com>
2016-12-05 20:40:52 -0500

    tabs should work now, (though not sure yet because I couldn't run it on my emulator due to an AVD/SDK version incompatibility)

--- app/src/main/java/com/halliday/jack/draganddropchinese/DictionaryActivity.java
+++ app/src/main/java/com/halliday/jack/draganddropchinese/DictionaryActivity.java
@@ -1,5 +1,6 @@
 package com.halliday.jack.draganddropchinese;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v4.app.Fragment;
@@ -9,6 +10,10 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
+import android.view.View;
+import android.widget.TabHost;
+
+import java.util.HashMap;
 
 /**
  * Created by CharlieC on November/18/16.
@@ -27,18 +32,17 @@ public class DictionaryActivity extends FragmentActivity {
         Fragment user_dict_fragment = fm.findFragmentById(R.id.user_dictionary);
         Fragment lib_dict_fragment = fm.findFragmentById(R.id.library_dictionary);
 
-        if (lib_dict_fragment == null|| user_dict_fragment == null){
+        if (lib_dict_fragment == null || user_dict_fragment == null) {
             FragmentTransaction trans = fm.beginTransaction();
-            if (lib_dict_fragment == null){
+            if (lib_dict_fragment == null) {
                 mDictionaryListFragment = new DictionaryListFragment();
                 trans.add(R.id.library_dictionary, mDictionaryListFragment);
             }
-            if (user_dict_fragment == null){
+            if (user_dict_fragment == null) {
                 mUserListFragment = new UserListFragment();
                 trans.add(R.id.user_dictionary, mUserListFragment);
             }
             trans.commit();
         }
     }
-
 }
